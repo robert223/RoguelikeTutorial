f82ba368b080c44de55d5a08a49d2a54f0c6dc43
robert233 <70767798+robert223@users.noreply.github.com>
2022-02-07 11:01:46 -0800

    finished part 3: added basic dungeon worldgen

--- game_map.py
+++ game_map.py
@@ -15,9 +15,7 @@ class GameMap:
 
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
-        self.tiles = np.full((width, height), fill_value=tile_types.floor, order="F")
-
-        self.tiles[30:33, 22] = tile_types.wall
+        self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
     def in_bounds(self, x: int, y: int) -> bool:
         """Return True if x and y are inside of the bounds of this map."""
@@ -26,5 +24,6 @@ class GameMap:
     """
     Using the Console class’s tiles_rgb method, we can quickly render the entire map. This method proves much faster than using the console.print method that we use for the individual entities.
     """
+
     def render(self, console: Console) -> None:
         console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
