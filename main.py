1da6a19bfb5947231a0d75d67977d77ff5b380b1
robert233 <70767798+robert223@users.noreply.github.com>
2022-03-01 09:50:18 -0800

    finished part 7 - interface and mouse tracking

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import copy
 
 import tcod
 
+import color
 from engine import Engine
 import entity_factories
 from procgen import generate_dungeon
@@ -15,7 +16,7 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -42,6 +43,10 @@ def main() -> None:
     )
     engine.update_fov()
 
+    engine.message_log.add_message(
+        "Welcome to the dungeon of all time!", color.welcome_text
+    )
+
     # window management
     with tcod.context.new_terminal(
             screen_width,
@@ -54,8 +59,11 @@ def main() -> None:
 
         # game loop
         while True:
-            engine.render(console=root_console, context=context)
-            engine.event_handler.handle_events()
+            root_console.clear()
+            engine.event_handler.on_render(console=root_console)
+            context.present(root_console)
+
+            engine.event_handler.handle_events(context)
 
 
 if __name__ == "__main__":
